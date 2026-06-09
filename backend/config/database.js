80cbd36d0d0feb13fad03645a909725cf51f20d2
blopezjayes <blopez.jayes@mineduc.gt>
2026-06-09 10:53:15 -0600

    deploy first version

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,7 @@ dotenv.config();
 export const sequelize = new Sequelize(
   process.env.DB_NAME || "sigec",
   process.env.DB_USER || "root",
-  process.env.DB_PASSWORD || "",
+  process.env.DB_PASSWORD || "root",
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
